ea06aec969e2fe12a84aafa0353b874a80b1bce3
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-08-05 13:05:45 +0900

    test: H2 DB 연동 및 엔티티 조회/수정 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,11 +6,26 @@ public class JpaMain {
 
     public static void main(String[] args) {
 
+        // 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유된다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        // 엔티티 매니저는 스레드 간에 절대 공유하면 안 된다. 사용하고 close()로 버려야 한다.
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("박병욱");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
