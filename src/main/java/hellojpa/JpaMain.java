d019a44cf92f515a265bbb1e1bf941acbe0bdade
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-08-08 17:00:02 +0900

    feat: @Table의 name 속성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,19 +6,20 @@ public class JpaMain {
 
     public static void main(String[] args) {
 
-        // 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유된다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
-        // 엔티티 매니저는 스레드 간에 절대 공유하면 안 된다. 사용하고 close()로 버려야 한다.
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("박병욱");
 
+            Member member = em.find(Member.class, 1L);
+            member.setName("사용자1");
+
+            em.flush();
+
+            System.out.println("===============================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,13 +2,22 @@ package hellojpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "mbr")
 public class Member {
 
     @Id
     private Long id;
     private String name;
+
+    public Member() {};
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
