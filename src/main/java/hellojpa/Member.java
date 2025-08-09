2cadf657ae8755406ecf37f4cfa341ffa4c1ac01
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-08-10 00:13:49 +0900

    feat: TABLE 전략 사용

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,11 +5,11 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
+@TableGenerator(name = "member_seq_generator", table = "my_sequences", pkColumnName = "member_seq", allocationSize = 1)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "member_seq_generator")
     private Long id;
 
     @Column(name = "name", nullable = false)
