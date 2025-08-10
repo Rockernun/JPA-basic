6a3f54afce9f03f0829e22b836bb4526f161441b
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-08-10 13:36:46 +0900

    test: 연관 관계의 주인에 값 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,16 +16,17 @@ public class JpaMain {
 
         try {
 
-            // 멤버 하나 만들고,
-            Member member = new Member();
-            member.setUsername("Member 1");
-            em.persist(member);
-
             // 팀 하나 만들고,
             Team team = new Team();
             team.setName("Team A");
-            team.getMembers().add(member);  // 멤버를 추가
+//            team.getMembers().add(member);  // 멤버를 추가
             em.persist(team);
+
+            // 멤버 하나 만들고,
+            Member member = new Member();
+            member.setUsername("Member 1");
+            member.setTeam(team);  // 연관 관계의 주인에 값을 추가
+            em.persist(member);
 
             em.flush();
             em.clear();
